8ca614b44610259f32faf29474c0a3fb55857525
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-11-10 17:05:05 +0000

    test plan for dictionarystore started

---
+++ test/app/lib/DictionaryStore.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('DictionaryStore', function () {
+    describe('constructor', function () {
+        it.skip('should construct', function (asyncDone) {
+            asyncDone(new Error('flag'));
+        });
+    });
+    describe.skip('willSet', function () {
+
+    });
+    describe.skip('willDelete', function () {
+
+    });
+    describe.skip('willGet', function () {
+
+    });
+    describe.skip('willGetCollection', function () {
+
+    });
+});

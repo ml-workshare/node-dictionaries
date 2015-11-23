450da9b99891f374b9faf28183ea6a4937457080
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-11-23 12:08:37 +0000

    DictionaryStore test removed .only

--- test/app/lib/DictionaryStore.spec.js
+++ test/app/lib/DictionaryStore.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe.only('DictionaryStore', function () {
+describe('DictionaryStore', function () {
     var DictionaryStore = require('../../../app/lib/DictionaryStore'),
         accountsCollection = {},
         usersCollection = {
